5e8081f8e6795aacfabe13a0ca16ad100f44a380
Ayengar <72708903+Ayengar@users.noreply.github.com>
2020-11-22 05:35:04 +0300

    Update instacheck.py

--- instacheck.py
+++ instacheck.py
@@ -1,9 +1,10 @@
+import os
 from telethon import TelegramClient, events
 from instagramy import InstagramUser
 
-BOT_TOKEN = ''
-API_ID = 
-API_HASH = ''
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
